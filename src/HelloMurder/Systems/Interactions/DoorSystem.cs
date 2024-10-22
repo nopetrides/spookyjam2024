0586f2b9758f2a73306c1a28baaad3a26c5b27ca
Matthew Stevens <mstevens@llst.ca>
2024-10-21 20:50:09 -0700

    doors open

---
+++ src/HelloMurder/Systems/Interactions/DoorSystem.cs
@@ -0,0 +1,58 @@
+﻿using Bang;
+using Bang.Components;
+using Bang.Entities;
+using Bang.Systems;
+using HelloMurder.Components;
+using Murder.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMurder.Systems.Interactions
+{
+    [Watch(typeof(DoorComponent))]
+    internal class DoorSystem : IReactiveSystem
+    {
+        public void OnAdded(World world, ImmutableArray<Entity> entities)
+        {
+
+        }
+
+        public void OnModified(World world, ImmutableArray<Entity> entities)
+        {
+            GameLogger.Log("Door state changed");
+            UpdateDoors(entities);
+        }
+
+        private void UpdateDoors(ImmutableArray<Entity> entities)
+        {
+            foreach(var entity in entities)
+            {
+                var door = entity.GetComponent<DoorComponent>();
+                if (door.IsOpen)
+                {
+                    OpenDoor(entity);
+                    continue;
+                }
+                CloseDoor(entity);
+            }
+        }
+
+        private void OpenDoor(Entity entity)
+        {
+            entity.Deactivate();
+        }
+        private void CloseDoor(Entity entity)
+        {
+            entity.Activate();
+        }
+
+        public void OnRemoved(World world, ImmutableArray<Entity> entities)
+        {
+
+        }
+    }
+}
