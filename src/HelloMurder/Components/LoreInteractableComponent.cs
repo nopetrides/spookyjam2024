5ad75f7a496141c9501f3bccc18ef67825491a71
Matthew Stevens <mstevens@llst.ca>
2024-10-28 21:32:05 -0700

    added very basic lore viewing

---
+++ src/HelloMurder/Components/LoreInteractableComponent.cs
@@ -0,0 +1,20 @@
+﻿using Bang.Components;
+using HelloMurder.Systems.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMurder.Components
+{
+    public readonly struct LoreInteractableComponent : IComponent
+    {
+        public readonly string LoreSnippet = "Enter your lore dump here";
+        public readonly LoreScreenSide ScreenSide = LoreScreenSide.Left;
+
+        public LoreInteractableComponent()
+        {
+        }
+    }
+}
